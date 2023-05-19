de0b06529c023e50577f0e721096a78d477f6a1c
Denys Pyshniuk <denys.pyshniuk@gmail.com>
2023-05-19 12:58:28 -0700

    Combine logic for validation from the server to the script

--- public/script.js
+++ public/script.js
@@ -1,35 +1,44 @@
-const url = "http://localhost:3001/validate-input";
 const messageArea = document.querySelector("#message-area");
 const form = document.querySelector("#input-form");
 const inputArea = document.querySelector("#input");
 
 // Submit button
 form.addEventListener("submit", async (event) => {
-  event.preventDefault(); // Prevent default form submission behavior
+  event.preventDefault();
   messageArea.innerHTML = "";
 
   const formData = new FormData(form);
   const input = formData.get("input");
-  const data = { input };
-  const response = await fetch(url, {
-    method: "post",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  });
-  const result = await response.json();
-
-  if (result.message && result.status === "200") {
-    messageArea.style.color = "green";
-    messageArea.append(result.message);
-  } else if (result.message && result.status === "400") {
-    messageArea.style.color = "orange";
-    messageArea.append(result.message);
-  } else {
+  if (!input || input.length !== 9 || isNaN(Number(input))) {
     messageArea.style.color = "red";
-    messageArea.append(result.error);
-  }
+    messageArea.append("Invalid input! Please make sure that you are entering 9 digits number without spaces.");
+  } else {
+      const digits = input.split('').map(Number);
+      let sum = 0;
+
+      for (let i = 0; i < digits.length; i++) {
+        let digit = digits[i];
+
+        if (i % 2 === 1) {
+          digit *= 2;
+
+          if (digit > 9) {
+            digit = digit.toString().split('').map(Number).reduce((a, b) => a + b);
+          }
+        }
+        sum += digit;
+      }
+
+      const isValid = sum % 10 === 0;
+
+      if(isValid) {
+        messageArea.style.color = "green";
+        messageArea.append("SIN is VALID.");
+      } else {
+        messageArea.style.color = "orange";
+        messageArea.append("SIN is INVALID.");
+      };
+    }
 });
 
 // Input text color change and remove message
