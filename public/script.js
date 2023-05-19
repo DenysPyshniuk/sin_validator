078572d439509f9d5a8ca19ba0f81f725daefd2c
Denys Pyshniuk <denys.pyshniuk@gmail.com>
2023-05-19 13:43:42 -0700

    Clean up and separate helper functions

--- public/script.js
+++ public/script.js
@@ -2,55 +2,74 @@ const messageArea = document.querySelector("#message-area");
 const form = document.querySelector("#input-form");
 const inputArea = document.querySelector("#input");
 
-// Submit button
-form.addEventListener("submit", async (event) => {
-  event.preventDefault();
-  messageArea.innerHTML = "";
-
-  const formData = new FormData(form);
-  const input = formData.get("input");
-  if (!input || input.length !== 9 || isNaN(Number(input))) {
-    messageArea.style.color = "red";
-    messageArea.append("Invalid input! Please make sure that you are entering 9 digits number without spaces.");
-  } else {
-      const digits = input.split('').map(Number);
-      let sum = 0;
-
-      for (let i = 0; i < digits.length; i++) {
-        let digit = digits[i];
-
-        if (i % 2 === 1) {
-          digit *= 2;
-
-          if (digit > 9) {
-            digit = digit.toString().split('').map(Number).reduce((a, b) => a + b);
-          }
-        }
-        sum += digit;
-      }
-
-      const isValid = sum % 10 === 0;
-
-      if(isValid) {
-        messageArea.style.color = "green";
-        messageArea.append("SIN is VALID.");
-      } else {
-        messageArea.style.color = "orange";
-        messageArea.append("SIN is INVALID.");
-      };
-    }
-});
-
 // Input text color change and remove message
 inputArea.addEventListener("input", () => {
-  messageArea.innerHTML = "";
+  resetMessageArea();
   const data = inputArea.value;
-  data.length === 9
-    ? (inputArea.style.color = "blue")
-    : (inputArea.style.color = "red");
+  inputArea.style.color = data.length === 9 ? "blue" : "red";
 });
 
 // Clear message when input gains focus
 inputArea.addEventListener("focus", () => {
-  messageArea.innerHTML = "";
+  resetMessageArea();
 });
+
+// Submit button
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  resetMessageArea();
+
+  const formData = new FormData(form);
+  const input = formData.get("input");
+  if (!isValidInput(input)) {
+    showMessage(
+      "Invalid input! Please make sure that you are entering a 9-digit number without spaces.",
+      "red"
+    );
+  } else {
+    const isValid = isValidSIN(input);
+
+    showMessage(
+      isValid ? "SIN is VALID." : "SIN is INVALID.",
+      isValid ? "green" : "orange"
+    );
+  }
+});
+
+//Helper Functions
+function isValidInput(input) {
+  return input && input.length === 9 && /^\d+$/.test(input);
+}
+
+function isValidSIN(sin) {
+  const digits = sin.split("").map(Number);
+    let sum = 0;
+
+    for (let i = 0; i < digits.length; i++) {
+      let digit = digits[i];
+
+      if (i % 2 === 1) {
+        digit *= 2;
+
+        if (digit > 9) {
+          digit = digit
+            .toString()
+            .split("")
+            .map(Number)
+            .reduce((a, b) => a + b);
+        }
+      }
+      digits[i] = digit;
+      sum += digit;
+    }
+    return sum % 10 === 0;
+}
+
+function showMessage(message, color) {
+  messageArea.style.color = color;
+  messageArea.append(message);
+}
+
+function resetMessageArea() {
+  messageArea.innerHTML = "";
+}
